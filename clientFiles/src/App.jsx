883bd45077ad69f7a8a255327d7a3d65959f92de
Shahmeer Mondal <shahmeermondal1576@gmail.com>
2025-06-03 21:55:40 +0530

    Updated App.jsx - fixed default retrive route

--- clientFiles/src/App.jsx
+++ clientFiles/src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Upload from "./pages/Upload";
@@ -5,13 +6,27 @@ import Retrieve from "./pages/Retrieve";
 import About from "./pages/About";
 
 function App() {
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    const handleLoad = () => setLoaded(true);
+    if (document.readyState === "complete") {
+      setLoaded(true);
+    } else {
+      window.addEventListener("load", handleLoad);
+      return () => window.removeEventListener("load", handleLoad);
+    }
+  }, []);
+
+  if (!loaded) return null;
   return (
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/upload" element={<Upload />} />
-          <Route path="/retrieve/:code" element={<Retrieve />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/upload" element={<Upload />} />
+      <Route path="/retrieve" element={<Retrieve />} />
+      <Route path="/retrieve/:code" element={<Retrieve />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
   );
 }
 
