469797ee60591301743b5acd66c4afab10a70600
shahmeer <shahmeer@kali.none>
2025-06-25 13:31:25 +0530

    Updated Styling

--- clientFiles/src/pages/Home.jsx
+++ clientFiles/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { useNavigate } from "react-router-dom";
 import style from "../styles/styling.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
