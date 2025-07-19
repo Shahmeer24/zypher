dceeba141a28e5fce1c85bf48a5c9336a51294ee
shahmeer <shahmeer@kali.none>
2025-07-19 23:29:27 +0530

    Updated Structure

--- clientFiles/src/config.js
+++ clientFiles/src/config.js
@@ -2,4 +2,4 @@ const isDev = window.location.hostname === "localhost";
 
 export const BASE_URL = isDev
   ? "http://localhost:5000"
-  : "zypher-24-backend.onrender.com";
+  : "https://zypher-24-backend.onrender.com";
